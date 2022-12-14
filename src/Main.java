48c1deade31b55588ff1a957e71f3a42850a0157
James McGauran <jamesmcgauran11@gmail.com>
2022-12-14 12:50:35 +0000

    Build out word count indexing lab as a starting point

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import indexer.WordIndexerService;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Word Indexer using virtual threads");
+
+        // Create a WordIndexerService
+        WordIndexerService wordIndexerService = new WordIndexerService();
+        // Index the files in the directory
+        wordIndexerService.indexFile("./1984_orwell.txt", "output.txt");
     }
 }
